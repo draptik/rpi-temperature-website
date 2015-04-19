0c1f2abf2891aa28c5001e2403e2cb28e263636a
Patrick Drechsler <patrick@pdrechsler.de>
2015-04-19 20:13:33 +0200

    started adding backend (sqlite3). Doesn't work yet

--- app_api/controllers/temperatures.js
+++ app_api/controllers/temperatures.js
@@ -1,4 +1,4 @@
-
+var temperatureBackend = require('../models/db');
 
 var sendJSONresponse = function(res, status, content) {
 	res.status(status);
@@ -7,23 +7,6 @@ var sendJSONresponse = function(res, status, content) {
 
 module.exports.index = function(req, res) {
 	console.log('API temperatures.index called..');
-	// TODO Replace with DB call:
-	var tempData = [
-		{degreeCelsius: 19.0, sensorName: 'Z2', timestamp: '2015-01-01 12:00:00'},
-		{degreeCelsius: 19.0, sensorName: 'WU', timestamp: '2015-01-01 12:05:00'},
-		{degreeCelsius: 19.0, sensorName: 'WU', timestamp: '2015-01-01 12:10:00'},
-		{degreeCelsius: 19.0, sensorName: 'WU', timestamp: '2015-01-01 12:15:00'},
-		{degreeCelsius: 19.0, sensorName: 'WU', timestamp: '2015-01-01 12:20:00'},
-		{degreeCelsius: 20.0, sensorName: 'Z2', timestamp: '2015-01-01 13:00:00'},
-		{degreeCelsius: 19.0, sensorName: 'WU', timestamp: '2015-01-01 13:05:00'},
-		{degreeCelsius: 19.0, sensorName: 'WU', timestamp: '2015-01-01 13:10:00'},
-		{degreeCelsius: 19.0, sensorName: 'WU', timestamp: '2015-01-01 13:15:00'},
-		{degreeCelsius: 19.0, sensorName: 'WU', timestamp: '2015-01-01 13:20:00'},
-		{degreeCelsius: 21.0, sensorName: 'Z2', timestamp: '2015-01-01 14:00:00'},
-		{degreeCelsius: 19.0, sensorName: 'WU', timestamp: '2015-01-01 14:05:00'},
-		{degreeCelsius: 19.0, sensorName: 'WU', timestamp: '2015-01-01 14:10:00'},
-		{degreeCelsius: 19.0, sensorName: 'WU', timestamp: '2015-01-01 14:15:00'},
-		{degreeCelsius: 19.0, sensorName: 'WU', timestamp: '2015-01-01 14:20:00'}
-	];
+	var tempData = temperatureBackend.getAll();
 	sendJSONresponse(res, 200, tempData);
 };
--- app_api/models/db.js
+++ app_api/models/db.js
@@ -1,15 +1,55 @@
 var sqlite3 = require('sqlite3');
-var db = new sqlite3.Database('templog.sqlite3');
+var fs = require('fs');
+var file = 'sample_data/templog.db';
+var exists = fs.existsSync(file);
+
+if (!exists) {
+	console.log('file does not exist.');
+} else {
+	console.log('file exists.');
+}
+
+var db = new sqlite3.Database(file);
 
 module.exports.getAll = function () {
-	db.run(sqlQuery, function (err) {
+console.log('3');
+	var tempData = [];
+	// TODO Replace with DB call:
+	// tempData = [
+	// 	{degreeCelsius: 19.0, sensorName: 'Z2', timestamp: '2015-01-01 12:00:00'},
+	// 	{degreeCelsius: 19.0, sensorName: 'WU', timestamp: '2015-01-01 12:05:00'},
+	// 	{degreeCelsius: 19.0, sensorName: 'WU', timestamp: '2015-01-01 12:10:00'},
+	// 	{degreeCelsius: 19.0, sensorName: 'WU', timestamp: '2015-01-01 12:15:00'},
+	// 	{degreeCelsius: 19.0, sensorName: 'WU', timestamp: '2015-01-01 12:20:00'},
+	// 	{degreeCelsius: 20.0, sensorName: 'Z2', timestamp: '2015-01-01 13:00:00'},
+	// 	{degreeCelsius: 19.0, sensorName: 'WU', timestamp: '2015-01-01 13:05:00'},
+	// 	{degreeCelsius: 19.0, sensorName: 'WU', timestamp: '2015-01-01 13:10:00'},
+	// 	{degreeCelsius: 19.0, sensorName: 'WU', timestamp: '2015-01-01 13:15:00'},
+	// 	{degreeCelsius: 19.0, sensorName: 'WU', timestamp: '2015-01-01 13:20:00'},
+	// 	{degreeCelsius: 21.0, sensorName: 'Z2', timestamp: '2015-01-01 14:00:00'},
+	// 	{degreeCelsius: 19.0, sensorName: 'WU', timestamp: '2015-01-01 14:05:00'},
+	// 	{degreeCelsius: 19.0, sensorName: 'WU', timestamp: '2015-01-01 14:10:00'},
+	// 	{degreeCelsius: 19.0, sensorName: 'WU', timestamp: '2015-01-01 14:15:00'},
+	// 	{degreeCelsius: 19.0, sensorName: 'WU', timestamp: '2015-01-01 14:20:00'}
+	// ];
+	// return tempData;
+
+	var sqlQuery = 'SELECT * FROM temps';
+	
+	db.each(sqlQuery, function (err, row) {
 		if (err !== null) {
 			// TODO Error handling
 			console.log('Error during sql query: ' + err);
 		} else {
 			// TODO Return collection from db as json object.
-			console.log('No error return from sql query.');
+			tempData.push({
+				degreeCelsius: row.temp,
+				sensorName: row.ID,
+				timestamp: row.timestamp
+			});
 		}
 	});
+
+	return tempData;
 };
 
