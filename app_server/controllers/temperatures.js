26bd4b3228aa9c12301a8e99c99007a904bd5586
Patrick Drechsler <socialcoding@pdrechsler.de>
2015-04-27 21:19:50 +0200

    Removed duplicate and unneeded api call from temperatures controller in express.

--- app_server/controllers/temperatures.js
+++ app_server/controllers/temperatures.js
@@ -1,52 +1,10 @@
 
-var request = require('request');
-var apiOptions = {
-	server: 'http://localhost:3000'
-};
-
-if (process.env.NODE_ENV === 'production') {
-	apiOptions.server = 'https://your-live-server-for-this-app,com';
-}
-
-
-var renderPage = function (req, res, responseBody) {
-	var message;
-
-	if (!(responseBody instanceof Array)) {
-		message = 'API lookup error';
-		responseBody = [];
-	} else {
-		if (!responseBody.length) {
-			message = 'No temperatures found.';
-		}
-	}
-	
-	res.render('temperatures-list', {
-		title: 'Some title',
-		pageHeader: {
-			title: 'Temperatures'
-		},
-		temperatures: responseBody,
-		message: message
-	});
-};
-
 /* GET temperatures page */
 module.exports.index = function(req, res, next) {
-	var requestOptions, path;
-	path = '/api/temperatures';
-	requestOptions = {
-		url: apiOptions.server + path,
-		method: 'GET',
-		json: {}
-	};
-
-	request(requestOptions, function (err, response, body) {
-		var i, data;
-		data = body;
-		if (response.statusCode !== 200) {
-			console.log('Status code not 200.');
+	res.render('temperatures-list', {
+		title: 'RPi temperatures',
+		pageHeader: {
+			title: 'Temperatures'
 		}
-		renderPage(req, res, data);
 	});
 };
