c0a3de020c24ed8d3509f0e80fccd059962bd250
Patrick Drechsler <socialcoding@pdrechsler.de>
2015-05-02 21:53:44 +0200

    WIP. Added overview chart for zooming detail chart. Overview changes cursor style at selection borders. Added flot.crosshair lib.

--- public/javascripts/chart.js
+++ public/javascripts/chart.js
@@ -8,30 +8,148 @@ var renderChart = function(data) {
 
 	var flotdata = convertDataForFlot(data);
 
-	var options = {
+	var detailPlaceholder = $('#detail-placeholder');
+	var detailPlot = $.plot(detailPlaceholder, flotdata, {
 		xaxis: { mode: 'time' },
 		series: {
 			lines: { show: true },
-			points: { show: false }
+			points: { show: true }
 		},
 		legend: { show: true },
 		grid: {
 			hoverable: true,
 			clickable: true
 		},
-		selection: { mode: 'x' }
-	};
+		selection: { mode: 'x' },
+		crosshair: { mode: '' }
+	});
 
-	var placeholder = $('#placeholder');
-	var plot = $.plot(placeholder, flotdata, options);
-
-	showTooltip();
-	zoom(plot, placeholder);
-	pane(plot, placeholder);
+	var overviewPlaceholder = $('#overview-placeholder');
+	var overviewPlot = $.plot(overviewPlaceholder, flotdata, {
+		series: {
+			lines: { show: true, lineWidth: 1 },
+			shadowSize: 0,
+			points: { show: false }
+		},
+		xaxis: { ticks: [], mode: "time" },
+		yaxis: { ticks: [], min: 0, autoscaleMargin: 0.1 },
+		legend: { show: false },
+		grid: { hoverable: true },
+		selection: { mode: 'x' },
+		crosshair: { mode: 'x' }
+	});
+	
+	showTooltip(detailPlaceholder);
+	zoom(detailPlot, detailPlaceholder, overviewPlot, overviewPlaceholder);
+	pane(detailPlot, detailPlaceholder);
 };
 
-var pane = function (plot, placeholder) {
+var zoom = function (plot, placeholder, overview, overviewPlaceholder) {
+
+	var min = plot.getData()[0].data[0][0];
+	var lastEntryPos = plot.getData()[0].data.length - 1;
+	var max = plot.getData()[0].data[lastEntryPos][0];
+
+	var currentRange;
 	
+	// Zoom in (only x axis)
+	placeholder.bind('plotselected', function (event, ranges) {
+		$.each(plot.getXAxes(), function (_, axis) {
+			var opts = axis.options;
+			opts.min = ranges.xaxis.from;
+			opts.max = ranges.xaxis.to;
+		});
+		plot.setupGrid();
+		plot.draw();
+		plot.clearSelection();
+
+		overview.setSelection(ranges, true);
+	});
+
+	overviewPlaceholder.bind('plotselected', function (event, ranges) {
+		console.log('ranges overview');
+		console.log(ranges.xaxis);
+		console.log('xaxis min: ' + overview.getXAxes()[0].min);
+		console.log('xaxis max: ' + overview.getXAxes()[0].max);
+		console.log(event);
+		currentRange = ranges.xaxis;
+		
+		plot.setSelection(ranges);
+	});
+
+	// hover range borders -------------------------------------------------------------------
+	var dragRangeInPixel = 10;
+	
+	$('<div id="dragCursor"></div>')
+		.appendTo(overviewPlaceholder)
+		.css({
+			cursor: 'col-resize',
+			position: 'absolute',
+			top: '6px',
+			width: dragRangeInPixel + 'px',
+			height: '88px',
+			border: '1px solid red', // <-- for debugging
+			display: 'none'
+		});
+		
+	// NOTE: To use 'plothover' the plot option 'grid: { hoverable: true }' is required!
+	overviewPlaceholder.bind('plothover', function (event, pos, item) {
+		if (currentRange) {
+			var from = overview.p2c({ x: currentRange.from, y: pos.y });
+			var to = overview.p2c({ x: currentRange.to, y: 0 });
+			var currentXPosPixel = overview.p2c({ x: pos.x, y: pos.y }).left;
+
+			var isInDragRange = function (currentX, targetX) {
+				return currentX >= (targetX.left - (dragRangeInPixel/2)) &&
+					currentX <= (targetX.left + (dragRangeInPixel/2));
+			};
+			
+			if (isInDragRange(currentXPosPixel, from)) {
+				$('#dragCursor').css({left: from.left + (dragRangeInPixel/2), display: '' });
+			} else if (isInDragRange(currentXPosPixel, to)) {
+				$('#dragCursor').css({left: to.left + (dragRangeInPixel/2), display: '' });
+			} else {
+				$('#dragCursor').hide();
+			}
+		}
+	});
+
+
+	// Zoom out button (back to origin size) --------------------------------------------
+	$("<div class='button' style='left:30px;bottom:40px'>zoom out</div>")
+		.appendTo(placeholder)
+		.click(function (event) {
+			event.preventDefault();
+			plot.setSelection({
+				xaxis: { from: min, to: max }
+			});
+		});
+};
+
+var showTooltip = function(placeholder){
+	$('<div id="tooltip"></div>').css({
+		position: 'absolute',
+		display: 'none',
+		border: '1px solid #fdd',
+		padding: '2px',
+		'background-color': '#fee',
+		opacity: 0.80
+	}).appendTo('body');
+	
+	placeholder.bind('plothover', function (event, pos, item) {
+		if (item) {
+			var x = item.datapoint[0],
+				y = item.datapoint[1].toFixed(1);
+			$('#tooltip').html(formatDate(x) + ' : <strong>' + y + '&deg;C</strong>')
+				.css({top: item.pageY+5, left: item.pageX+5})
+				.fadeIn(200);
+		} else {
+			$('#tooltip').hide();
+		}
+	});
+};
+ 
+var pane = function (plot, placeholder) {
 	function addArrow(dir, right, top, offset) {
 		$("<img class='button' src='/images/arrow-" + dir + ".gif' style='right:" + right + "px;top:" + top + "px'>")
 			.appendTo(placeholder)
@@ -45,58 +163,9 @@ var pane = function (plot, placeholder) {
 	addArrow('right', -20, 275, {left: 50});
 };
 
-var zoom = function (plot, placeholder) {
 
-	var min = plot.getData()[0].data[0][0];
-	var lastEntryPos = plot.getData()[0].data.length - 1;
-	var max = plot.getData()[0].data[lastEntryPos][0];
 
-	// Zoom in (only x axis)
-	placeholder.bind('plotselected', function (event, ranges) {
-		$.each(plot.getXAxes(), function (_, axis) {
-			var opts = axis.options;
-			opts.min = ranges.xaxis.from;
-			opts.max = ranges.xaxis.to;
-		});
-		plot.setupGrid();
-		plot.draw();
-		plot.clearSelection();
-	});
-
-	// Zoom out (back to origin size)
-	$("<div class='button' style='left:30px;bottom:40px'>zoom out</div>")
-		.appendTo(placeholder)
-		.click(function (event) {
-			event.preventDefault();
-			plot.setSelection({
-				xaxis: { from: min, to: max }
-			});
-		});
-};
-
-var showTooltip = function(){
-	$('<div id="tooltip"></div>').css({
-		position: 'absolute',
-		display: 'none',
-		border: '1px solid #fdd',
-		padding: '2px',
-		'background-color': '#fee',
-		opacity: 0.80
-	}).appendTo('body');
-	
-	$('#placeholder').bind('plothover', function (event, pos, item) {
-		if (item) {
-			var x = item.datapoint[0],
-				y = item.datapoint[1].toFixed(1);
-			$('#tooltip').html(formatDate(x) + ' : <strong>' + y + '&deg;C</strong>')
-				.css({top: item.pageY+5, left: item.pageX+5})
-				.fadeIn(200);
-		} else {
-			$('#tooltip').hide();
-		}
-	});
-};
-
+// Prepare data -------------------------------------------------------------------------------------------
 var formatDate = function(date) {
 	// requires moment.js
 	return moment.utc(new Date(date)).format('ddd, YYYY MMM DD HH:mm');
