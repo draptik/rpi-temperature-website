49e109f26805120f09e38ffc1d6bf0a4e48e614e
Patrick Drechsler <socialcoding@pdrechsler.de>
2015-06-15 06:28:12 +0200

    Extracted getByQuery.

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -13,56 +13,33 @@ if (!exists) {
 
 var db = new sqlite3.Database(dbLocation);
 
-module.exports.getAll = function (callback) {
+var getByQuery = function (query, cb) {
+    db.all(query, function (err, rows) {
+        if (err !== null) {
+            // TODO Error handling
+            console.log('Error during sql query: ' + err);
+        } else {
+            var temperatureData = [];
 
-    var sqlQuery = 'SELECT * FROM temps';
-
-    function getByQuery(query, cb) {
-        db.all(sqlQuery, function (err, rows) {
-            if (err !== null) {
-                // TODO Error handling
-                console.log('Error during sql query: ' + err);
-            } else {
-                var temperatureData = [];
-
-                for (var i = 0; i < rows.length; i++) {
-                    temperatureData.push({
-                        degreeCelsius: rows[i].temp,
-                        sensorName: rows[i].ID,
-                        timestamp: rows[i].timestamp
-                    });
-                }
-
-                cb(temperatureData);
+            for (var i = 0; i < rows.length; i++) {
+                temperatureData.push({
+                    degreeCelsius: rows[i].temp,
+                    sensorName: rows[i].ID,
+                    timestamp: rows[i].timestamp
+                });
             }
-        });
-    };
 
+            cb(temperatureData);
+        }
+    });
+};
+
+
+module.exports.getAll = function (callback) {
+    var sqlQuery = 'SELECT * FROM temps';
     getByQuery(sqlQuery, callback);
 };
 module.exports.getLastWeek = function (callback) {
     var sqlQuery = "SELECT * FROM temps WHERE timestamp BETWEEN datetime('now', '-7 days') AND datetime('now', 'localtime')";
-
-    function getByQuery(query, cb) {
-        db.all(sqlQuery, function (err, rows) {
-            if (err !== null) {
-                // TODO Error handling
-                console.log('Error during sql query: ' + err);
-            } else {
-                var temperatureData = [];
-
-                for (var i = 0; i < rows.length; i++) {
-                    temperatureData.push({
-                        degreeCelsius: rows[i].temp,
-                        sensorName: rows[i].ID,
-                        timestamp: rows[i].timestamp
-                    });
-                }
-
-                cb(temperatureData);
-            }
-        });
-    };
-
     getByQuery(sqlQuery, callback);
 };
