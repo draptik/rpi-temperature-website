a982c7511cd7ee83200b1d3f1f822a68853f988e
Patrick Drechsler <socialcoding@pdrechsler.de>
2015-11-06 22:02:17 +0100

    removed favicon from app.js

--- app.js
+++ app.js
@@ -19,8 +19,6 @@ var app = express();
 app.set('views', path.join(__dirname, 'app_server', 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
